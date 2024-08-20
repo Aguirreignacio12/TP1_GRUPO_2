cf07b73d563c3db8cb46d8b57f577c3821f949fc
Nahuel <Lehuan>
2024-08-20 16:13:20 -0300

    Agregue los protected y una persona mas

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -4,43 +4,43 @@ public class Empleado {
 	private final int _id;
 	private String _name;
 	private int _age;
-	private static int cont = 1000;
+	private static int cont = 1440;
 
-	public Empleado() {
+	protected Empleado() {
 		_id = cont;
 		cont++;
 		_name = "sin nombre";
 		_age = 99;
 	}
 
-	public Empleado(String name, int age) {
+	protected Empleado(String name, int age) {
 		_id = cont;
 		cont++;
 		this._name = name;
 		this._age = age;
 	}
 
-	public String get_name() {
+	protected String get_name() {
 		return _name;
 	}
 
-	public void set_name(String _name) {
+	protected void set_name(String _name) {
 		this._name = _name;
 	}
 
-	public int get_age() {
+	protected int get_age() {
 		return _age;
 	}
 
-	public void set_age(int _age) {
+	protected void set_age(int _age) {
 		this._age = _age;
 	}
 
-	public int get_id() {
+	protected int get_id() {
 		return _id;
 	}
 	
-	public static int devuelveProximoID() {
+	protected static int devuelveProximoID() {
 		return cont++;
 	}
 	//METHOD PUBLIC
--- src/EJERCICIO1/Principal.java
+++ src/EJERCICIO1/Principal.java
@@ -8,11 +8,13 @@ public class Principal{
 	Empleado y = new Empleado("Ignacio Aguirre", 30);
 	Empleado Z = new Empleado("Maria Lopez", 45);
 	Empleado A = new Empleado("Pedro Bazan", 20);
+	Empleado N = new Empleado("Lionel Messi", 37);
 		
 		System.out.println(x.toString());
 		System.out.println(y.toString());
 		System.out.println(Z.toString());
 		System.out.println(A.toString());
+		System.out.println(N.toString());
 		
 		System.out.println(Empleado.devuelveProximoID());
 	}
